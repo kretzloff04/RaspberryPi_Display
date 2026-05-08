86dbe6255a60864a523362262f6042ed9de92229
kretzloff04 <kretzloff04@gmail.com>
2026-05-08 12:20:17 -0500

    Added valid storage of expiration time

--- screens/running/strava_data.py
+++ screens/running/strava_data.py
@@ -12,21 +12,38 @@ STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
 STRAVA_CLIENT_ACCESS_TOKEN = os.getenv("STRAVA_CLIENT_ACCESS_TOKEN")
 STRAVA_REFRESH_TOKEN = os.getenv("STRAVA_REFRESH_TOKEN")
 
-curr_key_expire = 1778277600
 
-# key_url = "https://www.strava.com/oauth/token"
 
-# information = {
-#     "client_id": STRAVA_CLIENT_ID,
-#     "client_secret": STRAVA_CLIENT_SECRET,
-#     "code": 
-# }
+def read_expiration():
+    file_reader = open("key_expiration.txt", 'r')
+    string = file_reader.read()
+    float_expire = float(string)
+
+    file_reader.close()
+    print("[read_expiration] Key expiration read and stored in variable")
+    return float_expire
+
+curr_key_expire = read_expiration()
+
+def write_expiration(new_time):
+    file_writer = open("key_expiration.txt", 'w')
+    string_time = str(new_time)
+    file_writer.write(string_time)
+
+    print("[write_expiration] New time written to /key_expiration.txt")
+    file_writer.close()
+
 
 def get_key_json_data():
+
     if(time.time() > curr_key_expire):
         url = f"https://www.strava.com/oauth/token?client_id={STRAVA_CLIENT_ID}&client_secret={STRAVA_CLIENT_SECRET}&refresh_token={STRAVA_REFRESH_TOKEN}&grant_type=refresh_token"
         response = requests.post(url)
         data = response.json()
+
+        write_expiration(data["expires_at"])
+
+        print(f"[get_key_json_data] Updated expires time at to {data["expires_at"]}")
         print("[get_key_json_data] Access key expired, valid JSON request made")
         return data
     else:
@@ -36,6 +53,7 @@ def get_key_json_data():
 
 
 def get_new_token(key_json):
+    global STRAVA_CLIENT_ACCESS_TOKEN
     if(key_json == None):
         print("[get_new_token] Valid access token, no change")
         return None
